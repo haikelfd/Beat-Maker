1aa9925bceafcab599d76a21773df7f04eea20cb
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-04-25 12:25:04 -0700

    Adding sound

---
+++ app.js
@@ -0,0 +1,82 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.playBtn = document.querySelector(".play");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 150;
+    this.isPlaying = null; 
+  }
+  activePad() {
+    this.classList.toggle("active");
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    // Loop over the pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = ` playTrack 0.3s alternate ease-in-out 2`;
+      // Check if padfs are active
+      if (bar.classList.contains("active")) {
+        // Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    //Check if it's playing 
+    if(!this.isPlaying){
+        this.isPlaying = setInterval(() =>{
+            this.repeat();
+        }, interval);}
+        else{
+            //Clear the interval 
+            clearInterval(this.isPlaying);
+            console.log(this.isPlaying);
+         this.isPLaying = null;
+        }
+        }
+        updateBtn(){
+            if(!this.isPlaying){
+                this.playBtn.innerText = "stop";
+                this.playBtn.classList.add("active");
+            }else{
+                this.playBtn.innerText = "Play";
+                this.playBtn.classList.remove("active");
+
+            }
+            }
+    }
+    
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+const drumKit = new DrumKit();
+
+drumKit.pads.forEach((pad) => {
+  pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
+});
+
+drumKit.playBtn.addEventListener("click", function () {
+  drumKit.start();
+});
